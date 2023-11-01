4a72842230d849dc44892ebc1f19b8750ce40e8c
jahangir <jahangiralomtuf99@gmail.com>
2023-11-01 15:09:50 +0600

    resposnsive and total credit sum

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,25 +1,25 @@
 import Course from "../Course/Course";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Courses = ({allCourses, handleSelectBtn}) => {
-    return (
-        <div className="md:w-[75%]">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
-            {
-                    allCourses.map((course) => <Course
-                        key={course.id}
-                        course={course}
-                        handleSelectBtn={handleSelectBtn}
-                    ></Course>)
-           }
-           </div>
-        </div>
-    );
+const Courses = ({ allCourses, handleSelectBtn }) => {
+  return (
+    <div className="md:w-[75%]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
+        {allCourses.map((course) => (
+          <Course
+            key={course.id}
+            course={course}
+            handleSelectBtn={handleSelectBtn}
+          ></Course>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 Courses.propTypes = {
-    allCourses: PropTypes.array.isRequired,
-    handleSelectBtn: PropTypes.func.isRequired,
-  };
+  allCourses: PropTypes.array.isRequired,
+  handleSelectBtn: PropTypes.func.isRequired,
+};
 
 export default Courses;
--- src/components/CoursesName/CoursesName.jsx
+++ src/components/CoursesName/CoursesName.jsx
@@ -1,19 +1,26 @@
 import PropTypes from "prop-types";
 const CoursesName = ({ courseNames }) => {
-    let count = 1;
+  let count = 1;
   return (
-    <div className="">
+    <div className="md:sticky top-0 bg-[#FFF] h-[500px] p-4">
       <div className="border p-3">
         <h1 className="text-center text-2xl font-bold text-[#2F80ED]">
           Credit Hour Remaining 7 hr
-              </h1>
+        </h1>
         <hr className="border mt-2" />
       </div>
-          <h1 className="text-2xl font-bold ">Course Name</h1>
-          {
-              courseNames.map((name) => <p key={name.id}>{count++} {name.title}</p>)
-          }
-          <p>Total </p>
+      <h1 className="text-2xl font-bold ">Course Name</h1>
+      <div className="mt-6 text-[#1C1B1B99] space-y-2">
+        {courseNames.map((name) => (
+          <p key={name.id}>
+            {count++} {name.title}
+          </p>
+        ))}
+      </div>
+      <div className="space-y-4 mt-4">
+        <p className="font-bold">Total Credit Hour : 13 </p>
+        <h6 className="font-bold">Total Price : 48000 USD</h6>
+      </div>
     </div>
   );
 };

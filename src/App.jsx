c7b0016f8c5e4477872b2d2b2ab97407c29aa515
jahangir <jahangiralomtuf99@gmail.com>
2023-11-01 15:00:35 +0600

    show Course name and all titile name

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import { useEffect } from 'react';
 function App() {
 
   const [allCourses, setAllCourses] = useState([]);
+  const [courseNames, setCourseNames] = useState([])
+
+
   useEffect(() => {
     fetch('course.json')
       .then(res => res.json())
@@ -16,15 +19,21 @@ function App() {
 
 
   const handleSelectBtn = (course) => {
-    console.log("dffdfdfd");
+    const isExist = courseNames.find(item => item.id === course.id);
+    if (!isExist) {
+      setCourseNames([...courseNames, course])
+    }
+    else {
+      return alert("AllReady Exist this Name")
+    }
+    
   }
-
   return (
     <div className='bg-[#F3F3F3]'>
       <h1 className='text-4xl font-bold text-center'>Course Registration</h1>
       <div className='md:flex mt-12 container mx-auto'>
         <Courses allCourses={allCourses} handleSelectBtn={handleSelectBtn}></Courses>
-        <CoursesName></CoursesName>
+        <CoursesName courseNames={courseNames}></CoursesName>
       </div>
     </div>
   )
--- src/components/CoursesName/CoursesName.jsx
+++ src/components/CoursesName/CoursesName.jsx
@@ -1,10 +1,24 @@
-
-const CoursesName = () => {
-    return (
-        <div className="">
-            <h1>Course Name</h1>
-        </div>
-    );
+import PropTypes from "prop-types";
+const CoursesName = ({ courseNames }) => {
+    let count = 1;
+  return (
+    <div className="">
+      <div className="border p-3">
+        <h1 className="text-center text-2xl font-bold text-[#2F80ED]">
+          Credit Hour Remaining 7 hr
+              </h1>
+        <hr className="border mt-2" />
+      </div>
+          <h1 className="text-2xl font-bold ">Course Name</h1>
+          {
+              courseNames.map((name) => <p key={name.id}>{count++} {name.title}</p>)
+          }
+          <p>Total </p>
+    </div>
+  );
 };
 
+CoursesName.propTypes = {
+  courseNames: PropTypes.array.isRequired,
+};
 export default CoursesName;

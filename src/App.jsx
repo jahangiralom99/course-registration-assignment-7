685b4e58111e3828160c0143bda890cf57529f5a
jahangir <jahangiralomtuf99@gmail.com>
2023-11-01 20:22:02 +0600

    credit hours and total price and reamining time added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
 
   const [allCourses, setAllCourses] = useState([]);
   const [courseNames, setCourseNames] = useState([])
-
+  const [price, setPrice] = useState(0)
+  const [credit, setCredit] = useState(0)
+  const [remainingTime, setRemainingTime] = useState(0)
 
   useEffect(() => {
     fetch('course.json')
@@ -17,23 +19,58 @@ function App() {
       .then(data => setAllCourses(data))
   },[])
 
-
+ 
   const handleSelectBtn = (course) => {
+    let sumPrice = course.price;
+    let sumCredit = course.credit
     const isExist = courseNames.find(item => item.id === course.id);
     if (!isExist) {
+      {
+        courseNames.forEach(item => {
+          sumPrice = sumPrice + item.price;
+          
+        })
+      }
       setCourseNames([...courseNames, course])
     }
     else {
       return alert("AllReady Exist this Name")
     }
+    courseNames.forEach(item => {
+      sumCredit =sumCredit + item.credit
+    })
+    if (credit < 20) {
+      const creditRem =20 - sumCredit;
+      setCredit(sumCredit)
+      if (creditRem > 0) {
+        setRemainingTime(creditRem)
+      }
+      else {
+        alert("Credit")
+      }
+    }
+    else {
+      return alert("Please enter")
+    }
     
+    // if (creditRem > 0 && creditRem > 20) {
+      
+    // }
+   
+    setPrice(sumPrice)
   }
+
   return (
     <div className='bg-[#F3F3F3]'>
       <h1 className='text-4xl font-bold text-center'>Course Registration</h1>
       <div className='md:flex mt-12 container mx-auto'>
         <Courses allCourses={allCourses} handleSelectBtn={handleSelectBtn}></Courses>
-        <CoursesName courseNames={courseNames}></CoursesName>
+        <CoursesName
+          courseNames={courseNames}
+          price={price}
+          credit={credit}
+          remainingTime={remainingTime}
+        ></CoursesName>
       </div>
     </div>
   )
--- src/components/CoursesName/CoursesName.jsx
+++ src/components/CoursesName/CoursesName.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
-const CoursesName = ({ courseNames }) => {
+const CoursesName = ({ courseNames ,price, credit, remainingTime}) => {
   let count = 1;
   return (
     <div className="md:sticky top-0 bg-[#FFF] h-[500px] p-4">
       <div className="border p-3">
         <h1 className="text-center text-2xl font-bold text-[#2F80ED]">
-          Credit Hour Remaining 7 hr
+          Credit Hour Remaining {remainingTime} hr
         </h1>
         <hr className="border mt-2" />
       </div>
@@ -18,14 +18,17 @@ const CoursesName = ({ courseNames }) => {
         ))}
       </div>
       <div className="space-y-4 mt-4">
-        <p className="font-bold">Total Credit Hour : 13 </p>
-        <h6 className="font-bold">Total Price : 48000 USD</h6>
+        <p className="font-bold">Total Credit Hour : {credit} </p>
+        <h6 className="font-bold">Total Price : {price} USD</h6>
       </div>
     </div>
   );
 };
 
 CoursesName.propTypes = {
-  courseNames: PropTypes.array.isRequired,
+    courseNames: PropTypes.array.isRequired,
+    price: PropTypes.number.isRequired,
+    credit: PropTypes.number.isRequired,
+    remainingTime: PropTypes.number.isRequired,
 };
 export default CoursesName;
